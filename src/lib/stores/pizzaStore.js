22101ec1788ccddd4e38a4536099f55195f853dc
Fernando <fjquintana@uc.cl>
2023-06-08 17:52:28 -0400

    add first PizzaCard implementation

---
+++ src/lib/stores/pizzaStore.js
@@ -0,0 +1,3 @@
+import { writable } from "svelte/store";
+
+export const chosenPizzaId = writable(-1);
--- src/routes/buy_pizza/+page.svelte
+++ src/routes/buy_pizza/+page.svelte
@@ -1,79 +1,59 @@
 <script>
-    import PizzeriaCard from "$lib/components/PizzeriaCard.svelte";
-    import { API_URL } from "$lib/global.js";
-    import { onMount } from "svelte";
+    import PizzeriasCardsList from '$lib/components/PizzeriasCardsList.svelte';
+    import PizzasCardList from '$lib/components/PizzasCardsList.svelte'
 
-    let loading = true;
-    let pizzerias = []
+    import { chosenPizzeriaId } from '$lib/stores/pizzeriaStore.js'
+    import { chosenPizzaId } from '$lib/stores/pizzaStore.js'
+    import { onDestroy } from "svelte";
+    import { get } from 'svelte/store';
 
-    onMount(async () => {
+    let pizzeriaChosen = false;
+    let pizzaChosen = false;
 
-        const pizzeriasFetch = await fetch(`${API_URL}/pizzerias`)
-        let rawPizzerias = (await pizzeriasFetch.json()).pizzerias;
-        pizzerias = rawPizzerias.map( (pizzeria) => {
-            return {
-                id: pizzeria.id,
-                name: pizzeria.name,
-                address: pizzeria.address,
-                area: pizzeria.area,
-                openingHours: pizzeria.opening_hour.slice(0, 5),
-                closingHours: pizzeria.closing_hour.slice(0, 5),
-                imgUrl: pizzeria.image_url,
-            }
-        })
-        loading = false;
+    chosenPizzeriaId.subscribe( (value) => {
+        pizzeriaChosen = value < 0 ? false : true;
     });
 
+    chosenPizzaId.subscribe( (value) => {
+        pizzaChosen = value < 0 ? false : true;
+    })
 
+    onDestroy( () => {
+        chosenPizzeriaId.update((n) => -1);
+        chosenPizzaId.update((n) => -1);
+    })
 </script>
 
 <div class="hero">
     <div class="hero-body">
         <div class="container has-text-centered">
-            <p class="title" id='ChoosePizzeriaTitle'>Elige tu Pizzeria</p>
+            <p class="title" id='HeroTitle'>Elige tu Pizzeria</p>
         </div>
     </div>
 </div>
 
-{#if loading}
-    <div class="loader"></div>
+{#if !pizzeriaChosen}
+    <PizzeriasCardsList/>
+{:else if !pizzaChosen}
+    <PizzasCardList/>
 {:else}
-<div class="columns is-centered is-multiline is-mobile is-variable is-8">
-    {#each pizzerias as props, i}
-    <div class="column is-narrow">
-        <PizzeriaCard {...props}/>
-    </div>
-    {/each}
-</div>
+    <h1>Elegidas pizzas y pizzerias</h1>
+    <p>Pizzeria ID: {get(chosenPizzeriaId)}</p>
+    <p>Pizza ID: {get(chosenPizzaId)}</p>
 {/if}
 
+
+
 <style>
-    @font-face {
+    /* @font-face {
         font-family: 'PizzaFont';
         font-size: 'Normal';
         font-weight: 400;
         src: url('/fonts/CHEESEPIZZA.ttf') format('truetype')
-    }
+    } */
 
-    #ChoosePizzeriaTitle {
+    #HeroTitle {
         /* font-family: 'PizzaFont'; */
         font-size: 70px;
-    }
-
-    .loader {
-        border: 16px solid #f3f3f3; /* Light grey */
-        border-top: 16px solid hsl(48, 100%, 67%); /* Blue */
-        border-radius: 50%;
-        width: 120px;
-        height: 120px;
-        animation: spin 2s linear infinite;
-        margin-left: auto;
-        margin-right: auto;
-        /* position: absolute; */
-    }
-
-    @keyframes spin {
-        0% { transform: rotate(0deg); }
-        100% { transform: rotate(360deg); }
     }
 </style>
